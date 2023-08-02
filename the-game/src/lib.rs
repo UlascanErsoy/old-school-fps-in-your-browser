256016c3785f117c410e0b8b6d9c87cd2ffb3e0c
Ulascan Ersoy <cake@pop-os.localdomain>
2023-08-01 22:19:06 -0400

    draw stuff to canvas

--- the-game/src/lib.rs
+++ the-game/src/lib.rs
@@ -16,33 +16,37 @@ pub struct Pixel {
     a: u8
 }
 
+#[wasm_bindgen]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct ImageData {
     width: usize,
     height: usize,
     data: Vec<u8>
 }
 
+#[repr(u8)]
 pub enum ImageError {
     OutOfBounds
 }
 
+#[wasm_bindgen]
 impl ImageData {
 
-    pub fn new(width: usize, height: usize) -> Self {
-        ImageData { width, height, data: vec![250; width * height * 4]}
+    pub fn new(width: usize, height: usize) -> ImageData {
+        ImageData { width, height, data: vec![255; width * height * 4]}
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, pixel: &Pixel) -> Result<(), ImageError> {
+    fn set_pixel(&mut self, x: usize, y: usize, pixel: &Pixel) -> Result<(), ImageError> {
         
-        log(&format!("{}", y * self.width * 4 + x));
+        //log(&format!("{}", y * self.width * 4 + x));
         if x > self.width || y > self.height {
             return Err(ImageError::OutOfBounds);
         }
         
-        self.data[(x * self.height  + y) * 4] = pixel.r;
-        self.data[(x * self.height  + y) * 4 + 1] = pixel.g;
-        self.data[(x * self.height  + y) * 4 + 2] = pixel.b;
-        self.data[(x * self.height  + y) * 4 + 3] = pixel.a;
+        self.data[(y * self.width + x) * 4] = pixel.r;
+        self.data[(y * self.width + x) * 4 + 1] = pixel.g;
+        self.data[(y * self.width + x) * 4 + 2] = pixel.b;
+        self.data[(y * self.width + x) * 4 + 3] = pixel.a;
 
         Ok(())
     }
@@ -51,15 +55,33 @@ impl ImageData {
         self.data.as_ptr()
     }
 
+    pub fn draw_test_image(&mut self, x: u32, y: u32) -> *const u8 {
+
+        self.data = vec![255; self.width * self.height * 4];
+        for index in x..x+30 {
+            for yindex in y..y+30 {
+                self.set_pixel(index as usize,
+                               yindex as usize,
+                               &Pixel { r: (250  * yindex as usize / self.height) as u8,
+                                        g: (250  * index as usize/ self.width) as u8,
+                                        b: 0, 
+                                        a: 255
+                                    }).ok(); 
+            }
+        }
+
+        self.as_ptr() 
+    }
 }
+
 #[wasm_bindgen]
 pub fn draw_image_data(w: u32, h: u32) -> *const u8 {
     
     let mut image = ImageData::new(w as usize, h as usize);
 
-    for index in 0..10 {
-        for yindex in 0..100 {
-            image.set_pixel(index, yindex, &Pixel { r: 250, g:0, b:0, a:255}).ok(); 
+    for index in 0..w {
+        for yindex in 0..h {
+            image.set_pixel(index as usize, yindex as usize, &Pixel { r:255 , g:255, b:0, a:255}).ok(); 
         }
     }
     image.as_ptr() 
--- the-game/www/index.js
+++ the-game/www/index.js
@@ -5,14 +5,40 @@ const canvas = document.getElementById("game-window");
 
 const ctx = canvas.getContext("2d");
 
-function draw_image(ctx, width, height) {
-    const pixelPtr = wasm.draw_image_data(width, height);
+let imageData = wasm.ImageData.new(320, 240);
+
+let x = 10;
+let y = 10;
+
+async function draw_image(ctx, width, height) {
+    const pixelPtr = imageData.draw_test_image(x, y); 
     const arr = new Uint8ClampedArray(memory.buffer, pixelPtr, width * 4 * height);
     
-    console.log("LEN:", arr);
+    //console.log("LEN:", arr);
     const img_data = new ImageData(arr, width, height);
-
     ctx.putImageData(img_data, 0,0);
+    x = (x + 1)%320;
+    //y = (y + 1)%100;
 }
 
-draw_image(ctx,100, 100);
+function changeResolution(canvas, scaleFactor) {
+    // Set up CSS size.
+    canvas.style.width = canvas.style.width || canvas.width + 'px';
+    canvas.style.height = canvas.style.height || canvas.height + 'px';
+
+    // Resize canvas and scale future draws.
+    canvas.width = Math.ceil(canvas.width * scaleFactor);
+    canvas.height = Math.ceil(canvas.height * scaleFactor);
+    var ctx = canvas.getContext('2d');
+    ctx.scale(scaleFactor, scaleFactor);
+}
+
+async function render() {
+    draw_image(ctx,320, 240);
+    setTimeout(render, 10);
+    //requestAnimationFrame(render);
+}
+
+changeResolution(canvas, 0.25); 
+render();
+
